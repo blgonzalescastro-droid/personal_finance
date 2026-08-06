436806df218496d061ed4cfe8301cfc8bc435a9c
blgonzalescastro-droid <blgonzalescastro@gmail.com>
2026-08-06 17:00:36 -0500

    Authentication

--- personal_finance/urls.py
+++ personal_finance/urls.py
@@ -10,9 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Authentication endpoints
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('api/auth/', include('authentication.urls')),
     # Modulos endpoints
     path('api/', include('cards.urls')),
     path('api/', include('transactions.urls')),
